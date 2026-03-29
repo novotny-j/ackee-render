fd6bbe5aef3ae0f05282dd53f82d22036f428f77
novotny-j <83480323+novotny-j@users.noreply.github.com>
2026-03-30 00:42:25 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
-export ACKEE_MONGODB="mongodb://${MONGODB_HOSTPORT}/${MONGODB_DATABASE}"
+# Pokud NENÍ nastavena ACKEE_MONGODB, zkusíme ji poskládat z dílčích proměnných
+if [ -z "$ACKEE_MONGODB" ]; then
+  export ACKEE_MONGODB="mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOSTPORT}/${MONGODB_DATABASE}?retryWrites=true&w=majority"
+fi
 
 exec "$@"
